1d0af9fd79d9418ea0ccd7a4ec89ce73ebe7ca7f
Adila Nur Lathifah <strawberrytata3031@gmail.com>
2023-06-19 19:13:57 +0700

    Membuat notif channel

--- app/src/main/java/org/assesment3/MainActivity.kt
+++ app/src/main/java/org/assesment3/MainActivity.kt
@@ -1,5 +1,9 @@
 package org.assesment3
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
@@ -8,8 +12,22 @@ import androidx.recyclerview.widget.RecyclerView
 import org.assesment3.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        const val CHANNEL_ID = "updater"
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = getString(R.string.channel_name)
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel(CHANNEL_ID, name, importance)
+            channel.description = getString(R.string.channel_desc)
+            val manager = getSystemService(Context.NOTIFICATION_SERVICE)
+                    as NotificationManager?
+            manager?.createNotificationChannel(channel)
+        }
     }
 }
